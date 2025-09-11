1403a903ab89a0caac6a3bbe664799a5fed65805
Santoshalimkar <santosh.sa4d@gmail.com>
2025-09-12 02:04:55 +0530

    Changes added

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,21 +1,9 @@
-import Aboutusheader from "@/components/Aboutuscomponets/Aboutusheader";
-import ContactForm from "@/components/Contactuscomponents/contact-form";
-import Newsletter from "@/components/Homecomponets/Newsletter";
-import React from "react";
-import Conatctus from "@/public/Homeasset/Conatctus.png"
-import Contactus2 from "@/public/Homeasset/Contactus2.png"
+import React from 'react'
+
 const page = () => {
   return (
-    <div>
-      <Aboutusheader
-        image1={Conatctus}
-        image2={Contactus2}
-        title={"Contact Us"}
-      />
-      <ContactForm />
-      <Newsletter />
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default page;
+export default page
