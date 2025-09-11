560e60e263104876f1ef81a6e27db31b6efac347
Santoshalimkar <santosh.sa4d@gmail.com>
2025-09-12 02:13:20 +0530

    fixed

--- app/wishlist/page.js
+++ app/wishlist/page.js
@@ -1,14 +1,9 @@
-import Aboutusheader from "@/components/Aboutuscomponets/Aboutusheader";
-import WishlistPage from "@/components/Wishlistcomponets/Wishlist-page";
-import React from "react";
-import wishlist from "@/public/Homeasset/wishlist.png"
-import wishlist2 from "@/public/Homeasset/Wishlist2.png"
-const page = () => {
-  return (
-    <div>
-     wishlist
-    </div>
-  );
-};
+import React from 'react'
 
-export default page;
+function page() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
